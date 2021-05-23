6ae68b7f7106f85c7306356bdd0b4be0bd585f26
MayurChawla <mr.mayurchawla@gmail.com>
2021-05-23 21:00:50 +0530

    created basic layout

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+  const [todoInput, setTodoInput] = useState("");
+  function TodoAddFunction(e) {
+    e.preventDefault();
+    console.log("Added : " + todoInput);
+    setTodoInput("");
+    document.getElementById("myAnchor").focus();
+  }
+  
   return (
     <div className="App">
+      <h1>To-Do List</h1>
+      <form onSubmit={TodoAddFunction}>
+        <input autoFocus id="myAnchor" className="todo-inputarea" 
+          value={todoInput} onChange={(e)=>setTodoInput(e.target.value)}
+          placeholder="Input text to add to list"></input>
+        <button type="submit" className="todo-addbutton">Add Item</button>
+        <p>{todoInput}</p>
+      </form>
+      
     </div>
   );
 }
